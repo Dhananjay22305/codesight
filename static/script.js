e7daf5926cbe17fc34bf3244c466a3d77f7133b4
Dhananjay22305 <bhoyar.dhananjay.22@gmail.com>
2026-01-16 11:10:28 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
         analyzeBtn.innerText = "ANALYZING...";
 
         try {
-            const response = await fetch("http://127.0.0.1:8000/analyze", {
+            const response = await fetch("/analyze", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ code: userCode })
